b9d7feba5c18d0c39e5eee7935459002f25f9851
Alex Lay <alexander.lay@stfc.ac.uk>
2019-03-18 11:39:17 +0000

    Convert image points to real space using calculated transforms

--- converter.py
+++ converter.py
@@ -1,80 +1,66 @@
-"""
-Reads a chessboard image from a camera to obtain calibration data.
-That data is then used to transform pixel coordinates in other images from that camera to real world positions in millimeters
-"""
-
 import numpy as np
-import cv2
-import math
+import cv2 as cv
 
-side_length = 5  # The sides of the chessboard squares in millimeters
-row_corners = 15  # The number of corners on a row in the chessboard image
-col_corners = 10  # The number of corners on a column in the chessboard image
-img = cv2.imread('mocked checkboard.png')  # The chessboard calibration image
+# Camera calibration parameters determined by checkers.py
+old_camera_matrix = np.array([[8.99155523e+03, 0.00000000e+00, 1.96472471e+03],
+                              [0.00000000e+00, 8.98921557e+03, 1.34033122e+03],
+                              [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
+rotate_vector = np.array([[-0.0068679],
+                          [-0.03979584],
+                          [1.5731318]])
+translate_vector = np.array([[34.7388702],
+                             [-27.7625536],
+                             [207.90678648]])
+distortion_coefficients = np.array([
+    [-7.36345426e-01,
+     2.66415181e+00,
+     -4.23351460e-03,
+     -2.60186137e-03,
+     -1.61495764e+01]])
+new_camera_matrix = np.array([[8.54052246e+03, 0.00000000e+00, 1.96340416e+03],
+                              [0.00000000e+00, 8.55686035e+03, 1.33381448e+03],
+                              [0.00000000e+00, 0.00000000e+00, 1.00000000e+00]])
 
-objp = np.zeros((row_corners * col_corners, 3), np.float32)
-objp[:, :2] = np.mgrid[0:col_corners*side_length:side_length, 0:row_corners*side_length:side_length].T.reshape(-1, 2)
+points = np.array([[[3584.902, 2468.0232]]], np.float32)
 
-grayscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-found, corners = cv2.findChessboardCorners(grayscale, (col_corners, row_corners))
+undistorted = cv.undistortPoints(points, old_camera_matrix, distortion_coefficients, P=new_camera_matrix)
+print(undistorted)
 
-if not found:
-    print('Unable to find chessboard in image')
-    exit()
+# Homography matrix using the distcor_01 grid, and the above distortion correction, determined by allign_grid_image.py
+homography_matrix = np.array([[1.00389426e+00,  -1.82376659e-03, -3.53075992e+01],
+                              [-6.70797235e-03,  1.00522456e+00, -1.69727256e+01],
+                              [-3.41044536e-06, -2.41122605e-06,  1.00000000e+00]])
 
-h, w = img.shape[:2]
-calibrated, camera_matrix, distortion_coefficients, rotate_vector, translate_vector = cv2.calibrateCamera([objp], [corners], (w, h), None, None)
-
-translate_vector = translate_vector[0]
-rotate_vector = rotate_vector[0]
-
-if not calibrated:
-    print('Unable to calibrate camera')
-    exit()
-
-
-print('Camera matrix:\n' + str(camera_matrix))
-print('Rotation vector:\n' + str(rotate_vector))
-print('Translation vector:\n' + str(translate_vector))
-
-rotate_matrix, _ = cv2.Rodrigues(rotate_vector)
-
-
-def groundprojectpoint(image_point, z=0.0):
-    '''
-    Projects a point in the image onto the given z-plane
-    Resulting coordinates are given in millimeters
-    :param image_point: a point on the camera pane as a (u,v) pair
-    :param z: the z plane to project the image point onto
-    :return: the points coordinates in 3D space
-    '''
-    inverse_rotate = np.linalg.inv(rotate_matrix)
-    inverse_camera = np.linalg.inv(camera_matrix)
-
-    uv_point = np.ones((3, 1))
-    uv_point[0][0] = image_point[0]
-    uv_point[1][0] = image_point[1]
-
-    temp_matrix = np.matmul(np.matmul(inverse_rotate, inverse_camera), uv_point)
-    temp_matrix2 = np.matmul(inverse_rotate, translate_vector)
-
-    s = (z + temp_matrix2[2, 0]) / temp_matrix[2, 0]
-    wc_point = np.matmul(inverse_rotate, (np.matmul(s * inverse_camera, uv_point) - translate_vector))
-
-    wc_point[2] = z
-
-    return wc_point
+flattened = cv.perspectiveTransform(undistorted, homography_matrix)
+print(flattened)
 
 
-def testpoints(point1, point2, expecteddistance):
-    a = groundprojectpoint(point1)
-    b = groundprojectpoint(point2)
-    distance = math.sqrt((a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2)
-    error = (abs(expecteddistance - distance) / expecteddistance) * 100
-    print('expected: {} got: {} error:{:.2f}%'.format(expecteddistance, distance, error))
+class Corners:
+    def __init__(self, top_left, top_right, bottom_left, bottom_right):
+        self.top_left = top_left
+        self.top_right = top_right
+        self.bottom_left = bottom_left
+        self.bottom_right = bottom_right
 
 
-testpoints((68, 62), (72, 2466), 115/2)  # distcor_01 - topleft to bottomleft
-testpoints((68, 62), (88, 62), 1/2)  # distcor_01 - topleft to right neighbour
-testpoints((68, 62), (3600, 78), 169/2)  # distcor_01 - topleft to topright
-testpoints((1800, 1283), (1822, 1283), 1/2)  # distcor_01 - horizontal neighbours near center
+def point_to_real(image_point, image_corners: Corners, real_corners: Corners):
+    real_x_range = real_corners.top_right[0] - real_corners.top_left[0]
+    real_y_range = real_corners.bottom_left[1] - real_corners.top_left[1]
+
+    pixel_x_range = image_corners.top_right[0] - image_corners.top_left[0]
+    pixel_y_range = image_corners.bottom_left[1] - image_corners.top_left[1]
+
+    x_fraction = (image_point[0] - image_corners.top_left[0]) / pixel_x_range
+    y_fraction = (image_point[1] - image_corners.top_left[1]) / pixel_y_range
+
+    x = real_corners.top_left[0] + x_fraction * real_x_range
+    y = real_corners.top_left[1] + y_fraction * real_y_range
+
+    return x, y
+
+
+image_corner_points = Corners((50, 50), (3599, 50), (50, 2465), (3599, 2465))
+space_corner_points = Corners((0, 0), (85, 0), (0, 58), (85, 58))
+
+location = point_to_real(flattened[0, 0], image_corner_points, space_corner_points)
+print(location)
