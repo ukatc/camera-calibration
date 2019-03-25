5696d8bd3bff87107cf1ba4e155892ac7724204f
Alex Lay <alexander.lay@stfc.ac.uk>
2019-03-25 11:35:46 +0000

    Simplify distance calculation in homography determination

--- camera_calibration/configuration.py
+++ camera_calibration/configuration.py
@@ -165,7 +165,7 @@ class Config:
         top_right = grid[-cols, 0]
 
         def distance(p1, p2):
-            return math.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+            return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
 
         border = 50
         grid_width = max(distance(top_left, top_right),
